a5df5de3465d88335d41552a98972c7771ea7a67
Lenan <renanyamao@gmail.com>
2024-06-22 18:06:36 -0300

    Added clothes

--- Assets/Scriptable Objects/Clothes.cs
+++ Assets/Scriptable Objects/Clothes.cs
@@ -2,18 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Clothes : MonoBehaviour
+public enum Slot { None, Head, Chest, Arm, Leg }
+[CreateAssetMenu(menuName = "Scriptable Objects/Clothes")]
+public class Clothes : ScriptableObject
 {
     // Scriptable object to create clothes
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Slot slot;
+    public string clotheName;
+    public float clothePrice;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Head")]
+    public Sprite face;
+    public Sprite hood;
+
+    [Header("Chest")]
+    public Sprite chest;
+    public Sprite shoulderL;
+    public Sprite shoulderR;
+    public Sprite pelvis;
+
+    [Header("Arm")]
+    public Sprite wristL;
+    public Sprite elbowL;
+    public Sprite wristR;
+    public Sprite elbowR;
+    [Header("Leg")]
+    public Sprite legL;
+    public Sprite bootL;
+    public Sprite legR;
+    public Sprite bootR;
+
 }
