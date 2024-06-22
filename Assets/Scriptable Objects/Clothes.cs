3af3a4f3245e85a65d4e1b7a038f2a1435b15cd5
Lenan <renanyamao@gmail.com>
2024-06-21 23:20:25 -0300

    Base structure

---
+++ Assets/Scriptable Objects/Clothes.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clothes : MonoBehaviour
+{
+    // Scriptable object to create clothes
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/InteractionSystem/Interactable.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    // Base for an interactable object
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
